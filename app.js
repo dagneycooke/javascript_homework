67775fb258cf7cc5f33f1372390cf46bf7b6ca25
Dagney Cooke <14912982+dagneycooke@users.noreply.github.com>
2020-03-01 18:23:11 -0800

    finished level 1 homework

--- app.js
+++ app.js
@@ -4,24 +4,27 @@ var sightings = data;
 // create variable for filter button id="filter-btn"
 var button = d3.select("#filter-btn");
 
-// Create a basic HTML web page or use the index.html file provided (we recommend building your own custom page!).
+// what to do when you click hte filter button
+button.on("click", function() {
+    // get input from datetime form
+    var input = d3.select("#datetime").property("value");
 
-
-// Using the UFO dataset provided in the form of an array of JavaScript objects, write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
-// Make sure you have a column for date/time, city, state, country, shape, and comment at the very least.
-// Use a date form in your HTML document and write JavaScript code that will listen for events and search through the date/time column to find rows that match user input.
-
-sightings.forEach(function(sighting) {
+    // find table
     var table = d3.select("#ufotable")
-    var newRow = table.append("tr")
 
-    newRow.append("td").text(sighting.datetime);
-    newRow.append("td").text(sighting.city);
-    newRow.append("td").text(sighting.state);
-    newRow.append("td").text(sighting.country);
-    newRow.append("td").text(sighting.shape);
-    newRow.append("td").text(sighting.durationMinutes);
-    newRow.append("td").text(sighting.comments);
+    // remove any rows that already exist in the body of the table
+    table.selectAll("tr").remove()
 
-    
+    // filter data based on input
+    var filteredData = sightings.filter(sighting => sighting.datetime === input);
+
+    // for each object in the filtered data, add it to the table
+    filteredData.forEach((thing) => {
+
+        var newRow = table.append("tr");
+        Object.entries(thing).forEach(([key, value]) => {
+            newRow.append("td").text(value);
+        });
+    });   
+  
 });
